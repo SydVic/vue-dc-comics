8b8d87e48b27cad5e7e7ab341c48e6b5a6f7f850
SydVic <fabrizio.bocca27@gmail.com>
2022-05-17 16:02:27 +0200

    aggiunto main base e prefooter base

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,38 @@
   <div id="app">
     <AppHeader />
     <main>qui andrà il contenuto</main>
+    <PreFooter />
     <!-- FOOTER -->
   </div>
 </template>
 
 <script>
 import AppHeader from "./components/AppHeader.vue";
+import PreFooter from "./components/PreFooter.vue";
 
 export default {
   name: "App",
   components: {
     AppHeader,
+    PreFooter,
   },
 };
 </script>
 
 <style lang="scss">
 @import "./style/common.scss";
+@import "./style/variables.scss";
 
 /* test*/
 body {
   background-color: #000000;
 }
+main {
+  color: $white;
+  height: $main-height;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+/*test*/
 </style>
---
+++ src/components/PreFooter.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="pre-footer">
+    <div class="pre-footer__container">icons</div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "PreFooter",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../style/variables.scss";
+@import "../style/mixin.scss";
+
+.pre-footer {
+  height: $pre-footer-height;
+  background-color: $primary-color;
+
+  &__container {
+    @include general-container-properties();
+    /*test*/
+    border: 1px solid white;
+    height: 100%;
+  }
+}
+</style>
