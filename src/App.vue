64c2b9d000228d948c89dcd96a93c62b89164f69
SydVic <fabrizio.bocca27@gmail.com>
2022-05-17 14:32:38 +0200

    prima impostazione heaader

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,30 @@
 <template>
-  <div>
-    
+  <div id="app">
+    <AppHeader />
+    <main>qui andrà il contenuto</main>
+    <!-- FOOTER -->
   </div>
 </template>
 
 <script>
-
+import AppHeader from "./components/AppHeader.vue";
 
 export default {
   name: "App",
   components: {
-
+    AppHeader,
   },
 };
 </script>
 
 <style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
 
+body {
+  background-color: #000000;
+}
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="header">
+    <div class="header__nav-container">prova</div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../style/variables.scss";
+@import "../style/mixin.scss";
+
+.header {
+  height: $header-height;
+  background-color: $white;
+
+  &__nav-container {
+    @include general-container-properties();
+    background-color: aqua;
+  }
+}
+</style>
