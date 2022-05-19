c8caf8ed9104dc8d86a9da5a11abd8ffdeb44e3d
SydVic <fabrizio.bocca27@gmail.com>
2022-05-19 09:18:05 +0200

    aggiunta lista icone e testo prefooter in modo dinamico

--- src/components/AppPrefooter.vue
+++ src/components/AppPrefooter.vue
@@ -2,25 +2,9 @@
   <section class="pre-footer">
     <div class="pre-footer__container">
       <ul class="icons-list">
-        <li>
-          <img src="../assets/img/buy-comics-digital-comics.png" alt="" />
-          <h3>Digital Comics</h3>
-        </li>
-        <li>
-          <img src="../assets/img/buy-comics-digital-comics.png" alt="" />
-          <h3>Digital Comics</h3>
-        </li>
-        <li>
-          <img src="../assets/img/buy-comics-digital-comics.png" alt="" />
-          <h3>Digital Comics</h3>
-        </li>
-        <li>
-          <img src="../assets/img/buy-comics-digital-comics.png" alt="" />
-          <h3>Digital Comics</h3>
-        </li>
-        <li>
-          <img src="../assets/img/buy-comics-digital-comics.png" alt="" />
-          <h3>Digital Comics</h3>
+        <li v-for="(item, index) in icons" :key="index">
+          <img :src="require(`../assets/img/${item.img}`)" alt="" />
+          <h3>{{ item.text }}</h3>
         </li>
       </ul>
     </div>
@@ -30,6 +14,32 @@
 <script>
 export default {
   name: "AppPrefooter",
+  data: function () {
+    return {
+      icons: [
+        {
+          img: "buy-comics-digital-comics.png",
+          text: "Digital Comics",
+        },
+        {
+          img: "buy-comics-merchandise.png",
+          text: "DC merchandise",
+        },
+        {
+          img: "buy-comics-subscriptions.png",
+          text: "Subscritpion",
+        },
+        {
+          img: "buy-comics-shop-locator.png",
+          text: "Comic Shop Locator",
+        },
+        {
+          img: "buy-dc-power-visa.svg",
+          text: "DC Power Visa",
+        },
+      ],
+    };
+  },
 };
 </script>
 
@@ -40,6 +50,7 @@ export default {
 
 .pre-footer {
   background-color: $primary-color;
+  padding: 2rem 0;
 
   &__container {
     @include general-container-properties();
@@ -49,6 +60,16 @@ export default {
 
       li {
         width: calc(100% / 5);
+        display: flex;
+        align-items: center;
+
+        img {
+          width: 20%;
+        }
+
+        h3 {
+          color: $white;
+        }
       }
     }
   }
